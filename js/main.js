6f1d04100511ce7051b24ce80197fb44ae82065c
vadimushka_d <vadimushka.d@gmail.com>
2022-07-14 18:52:58 +0300

    view pre end

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const lazyLoadInstance = new LazyLoad({});
+const TwigGallery = {
+    run: () => {
+        document.querySelector('.description-swiper').swiper.on('slideChange', function () {
+            lazyLoadInstance.update();
+        });
+    },
+    runLightbox: (selector) => {
+        let galleryList = [];
+        document.querySelectorAll(selector)?.forEach((swiperSlidePicture) => {
+            galleryList.push(swiperSlidePicture.dataset.full);
+        })
+        const lightbox = new FsLightbox();
+        lightbox.props.sources = galleryList;
+
+        document.querySelectorAll(selector).forEach((swiperSlidePicture, position) => {
+            swiperSlidePicture.addEventListener('click', (e) => {
+                e.preventDefault();
+                lightbox.open(position);
+            })
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    TwigGallery.run();
+    TwigGallery.runLightbox('.description-slide');
+    TwigGallery.runLightbox('.description-rent-slide');
+
+    document.querySelector('.description-info__more').addEventListener('click', (e) => {
+        e.preventDefault();
+        let tabButton = document.querySelector(`button${e.target.getAttribute('href')}`);
+        tabButton.scrollIntoView();
+        tabButton.dispatchEvent(new MouseEvent('click'));
+    })
+
+    document.querySelector('.description__button-form').addEventListener('click', (e) => {
+        e.preventDefault();
+        let container = document.querySelector(e.target.getAttribute('href'));
+        container.scrollIntoView();
+    })
+
+    $('.phone-mask').mask('+7 (000) 000-00-00');
+})
