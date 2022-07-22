8ec34a68fed035dee38782e80079160ed43226d9
vadimushka_d <vadimushka.d@gmail.com>
2022-07-22 16:59:41 +0300

    gulp

--- markup_dist/gulpfile.js
+++ markup_dist/gulpfile.js
@@ -1,9 +1,9 @@
 /* Основной модуль */
 import gulp from "gulp";
 /* Импорт путей */
-import { path } from "../gulp/config/path.js";
+import { path } from "./gulp/config/path.js";
 /* Импорт общих плагинов */
-import { plugins } from "../gulp/config/plugins.js";
+import { plugins } from "./gulp/config/plugins.js";
 
 /* Передаем значения в глобальную переменную */
 global.app = {
@@ -15,17 +15,17 @@ global.app = {
 }
 
 /* Импорт задач */
-import { copy } from "../gulp/tasks/copy.js";
-import { reset } from "../gulp/tasks/reset.js";
-import { html } from "../gulp/tasks/html.js";
-import { server } from "../gulp/tasks/server.js";
-import { scss } from "../gulp/tasks/scss.js";
-import { js } from "../gulp/tasks/js.js";
-import { images } from "../gulp/tasks/images.js";
-import { otfToTtf, ttfToWoff, fontsStyle } from "../gulp/tasks/fonts.js";
-import { svgSprive } from "../gulp/tasks/svgSprive.js";
-import { zip } from "../gulp/tasks/zip.js";
-import { ftp } from "../gulp/tasks/ftp.js";
+import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { server } from "./gulp/tasks/server.js";
+import { scss } from "./gulp/tasks/scss.js";
+import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprive } from "./gulp/tasks/svgSprive.js";
+import { zip } from "./gulp/tasks/zip.js";
+import { ftp } from "./gulp/tasks/ftp.js";
 
 /* наблюдатель за изменениями в файлах */
 function watcher() {
